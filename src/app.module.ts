b1bd65b83f5254586ce6f8e1b001147952b8c181
soban-khan <soban.khanf@gmail.com>
2025-05-03 19:49:42 +0530

    Integrated postgres and moved health check api to app controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TYPEORM_CONFIG } from './configs/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(TYPEORM_CONFIG)],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
